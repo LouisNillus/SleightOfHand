1af82ef0de45e0cd8950b56e0ebb1fdcd2abc8f2
Eliot <ringevaleliot@hotmail.fr>
2021-03-26 18:05:18 +0100

    Broken Road Anim (WIP)

---
+++ Assets/Scripts/ELR_Scripts/BrokenRoadTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrokenRoadTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.GetComponent<Player>() != null) 
+        {
+            this.GetComponentInParent<Animator>().SetTrigger("Go");
+            Destroy(this.gameObject);
+        }
+    }
+}
